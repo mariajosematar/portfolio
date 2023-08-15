fdd97eb98263154621cb4508fcff69cfd957b2da
Maria Jose Matar <matarmariajose@gmail.com>
2023-08-15 16:17:15 +0200

    third commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio2.jpg'
+import IMG2 from '../../assets/portfolio4.jpg'
+import IMG3 from '../../assets/portfolio5.png'
+import IMG4 from '../../assets/img3.jpg'
+
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Portfolio</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/mariajosematar" className='btn btn-primary' target='_blank'>Github</a>
+            <a href="https://github.com/mariajosematar/portfolio" className='btn' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Nest App</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/mariajosematar" className='btn btn-primary' target='_blank'>Github</a>
+            <a href="https://github.com/mariajosematar/nestApp" className='btn' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>API rest</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/mariajosematar" className='btn btn-primary' target='_blank'>Github</a>
+            <a href="https://github.com/mariajosematar/APIrest" className='btn' target='_blank'>Live Demo</a>
+          </div>  
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>Battleship Python Game</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/mariajosematar" className='btn btn-primary' target='_blank'>Github</a>
+            <a href="https://github.com/mariajosematar/Battleship-Python3" className='btn' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -10,76 +10,76 @@ const Services = () => {
 
       <div className="container services__container">
         <article className="services">
-           <div className="services__head">
+          <div className="services__head">
             <h3>UI/UX Design</h3>
-           </div>
+          </div>
 
-           <ul className="service__list">
+          <ul className="service__list">
             <li>
-               <BiCheck className='service__list-icon' />
-               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
-            <li> 
+            <li>
               <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
-             <BiCheck className='service__list-icon' />
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
-           </ul>
+          </ul>
         </article>
         <article className="services">
-           <div className="services__head">
+          <div className="services__head">
             <h3>Web Development</h3>
-           </div>
+          </div>
 
-           <ul className='service__list'>  
+          <ul className='service__list'>
             <li>
-             <BiCheck className='service__list-icon' /> 
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
-            </li>
-            <li> 
-             <BiCheck className='service__list-icon' />
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
-             <BiCheck className='service__list-icon' />
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
-             <BiCheck className='service__list-icon' />
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
-           </ul>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
+            </li>
+          </ul>
         </article>
         <article className="services">
-           <div className="services__head">
+          <div className="services__head">
             <h3>Content Creation</h3>
-           </div>
+          </div>
 
-           <ul className='service__list'>
-            <li> 
-             <BiCheck className='service__list-icon' />
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
-             <BiCheck className='service__list-icon' />
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
-             <BiCheck className='service__list-icon' />
+              <BiCheck className='service__list-icon' />
               <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
             <li>
-             <BiCheck className='service__list-icon' />
-             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
+              <BiCheck className='service__list-icon' />
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, facere iusto quas neque quo voluptate commodi fugit culpa repellat. Eveniet, excepturi eius autem et velit nemo minus sapiente molestiae reiciendis?</p>
             </li>
-           </ul>
+          </ul>
         </article>
       </div>
     </section>
