cbfbda136ba7bf18015020a0bc9ad1e3f7642471
Maria Jose Matar <matarmariajose@gmail.com>
2023-08-15 19:51:50 +0200

    five commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () =>  {
   return (
     <footer>
-      <a href="#" className='footer_logo'>EGATOR</a>
+      <a href="#" className='footer_logo'>María José Matar Arenas</a>
 
       <ul className='permalinks' >
         <li><a href="#">Home</a></li>
@@ -15,7 +15,7 @@ const Footer = () =>  {
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-
+      
       <div className="footer__copyright">
         <small>2023 &copy; All rights reserved. </small>
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header> 
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Maria Jose</h1>
+        <h1>María José Matar Arenas</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
