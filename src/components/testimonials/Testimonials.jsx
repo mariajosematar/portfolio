ece0efa5185534781e94c5d21346a238d2f1eaf8
Maria Jose Matar <matarmariajose@gmail.com>
2023-08-15 11:17:22 +0200

    first commit

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonials.css'
+
+const Testimonios = () => {
+  return (
+    <section id='testimonials'>Testimonials</section>
+  )
+}
+
+export default Testimonios
